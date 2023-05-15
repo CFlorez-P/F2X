e2b3f64545250d8b05de6af70183760cb2fbda1c
CFlorez-P <133244198+CFlorez-P@users.noreply.github.com>
2023-05-15 12:47:55 -0500

    Add files via upload

---
+++ PruebaTecnica.Domain/Common/DataServiceDomain.cs
@@ -0,0 +1,35 @@
+﻿using PruebaTecnica.Domain.Abstraction.Shared;
+using PruebaTecnica.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PruebaTecnica.Domain.Common
+{
+    public class DataServiceDomain : IDataServiceDomain
+    {
+        private readonly IDataService DataService;
+
+        public DataServiceDomain(IDataService dataService)
+        {
+            DataService = dataService;
+        }
+
+        public async Task<string> GetToken()
+        {
+            return await DataService.GetToken();
+        }
+
+        public async Task<List<Vehicle>> GetVehicleCollectionData(string token, string date)
+        {
+            return await DataService.GetVehicleCollectionData(token, date);
+        }
+
+        public async Task<List<Vehicle>> GetVehicleCountData(string token, string date)
+        {
+            return await DataService.GetVehicleCountData(token, date);
+        }
+    }
+}
---
+++ PruebaTecnica.Domain/Common/VehicleDomain.cs
@@ -0,0 +1,48 @@
+﻿using MediatR;
+using PruebaTecnica.Domain.Abstraction;
+using PruebaTecnica.Domain.Abstraction.Shared;
+using PruebaTecnica.Domain.Entities;
+using PruebaTecnica.Domain.Request;
+using PruebaTecnica.Domain.Response;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PruebaTecnica.Domain.Common
+{
+    public class VehicleDomain : IVehicleDomain
+    {
+        private readonly IVehicleRepository VehicleRepository;
+
+        public VehicleDomain(IVehicleRepository vehicleRepository)
+        {
+            VehicleRepository = vehicleRepository;
+        }
+
+        public List<Vehicle> GetAllVehicleData(VehicleDataRequest request)
+        {
+            return VehicleRepository.GetAllVehicleData(request);
+        }
+
+        public List<VehicleValueResponse> GetVehicleValueData()
+        {
+            return VehicleRepository.GetVehicleValueData();
+        }
+
+        public bool SaveVehicleData(List<Vehicle> vehicles)
+        {
+            return VehicleRepository.SaveVehicleData(vehicles);         
+        }
+        public List<string> GetAllStations()
+        {
+            return VehicleRepository.GetAllStations();
+        }
+        public List<string> GetAllDates()
+        {
+            return VehicleRepository.GetAllDates();
+        }
+
+    }
+}
