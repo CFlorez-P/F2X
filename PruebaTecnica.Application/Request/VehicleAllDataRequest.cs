7c86284052a3ba35b870d5b79e4478cddd6b65ad
CFlorez-P <133244198+CFlorez-P@users.noreply.github.com>
2023-05-15 12:48:34 -0500

    Add files via upload

---
+++ PruebaTecnica.Application/Request/VehicleAllDataRequest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PruebaTecnica.Application.Request
+{
+    public class VehicleAllDataRequest
+    {
+        public string? FechaDesde { get; set; }
+        public string? FechaHasta { get; set; }
+        public string? Estacion { get; set; }
+        public int PageIndex { get; set; }
+        public int PageSize { get; set;}
+    }
+}
